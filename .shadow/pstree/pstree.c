e6357ce9d599dab55b02d3dcb78c82f5506948f4
tracer-nju <tracer@nju.edu.cn>
2024-11-19 10:09:54 +0800

    Linux debian 6.1.0-26-arm64 #1 SMP Debian 6.1.112-1 (2024-09-30) aarch64 GNU/Linux  10:09:54 up  1:15,  1 user,  load average: 0.49, 0.52, 0.34

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <getopt.h>
 
-struct settings {
+struct settings
+{
   int is_show_pid;
-  int is_numeric_sort; 
+  int is_numeric_sort;
 };
 
 struct settings g_settings;
 
-int parse_args(int argc, char *argv[]) {
+int parse_args(int argc, char *argv[])
+{
   const struct option table[] = {
-    {"show-pids", no_argument, NULL, 'p'},
-    {"version", no_argument, NULL, 'v'},
-    {0, 0, NULL, 0},
+      {"show-pids", no_argument, NULL, 'p'},
+      {"version", no_argument, NULL, 'v'},
+      {0, 0, NULL, 0},
   };
   int o;
-  while((o = getopt_long(argc, argv, "-pv", table, NULL)) != -1) {
-    switch(o) {
-      case 'v': printf("hello\n"); break;
-      default: exit(-1);
+  while ((o = getopt_long(argc, argv, "-pv", table, NULL)) != -1)
+  {
+    switch (o)
+    {
+    case 'v':
+      printf("hello\n");
+      break;
+    default:
+      exit(-1);
     }
   }
   return 0;
 }
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
+int main(int argc, char *argv[])
+{
+  for (int i = 0; i < argc; i++)
+  {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
