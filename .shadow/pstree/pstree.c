f02a7f84566ced9cae58e04dd6bad6c93300b4e7
tracer-nju <tracer@nju.edu.cn>
2024-11-19 10:09:25 +0800

    Linux debian 6.1.0-26-arm64 #1 SMP Debian 6.1.112-1 (2024-09-30) aarch64 GNU/Linux  10:09:25 up  1:15,  1 user,  load average: 0.71, 0.56, 0.34

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <getopt.h>
+
+struct settings {
+  int is_show_pid;
+  int is_numeric_sort; 
+};
+
+struct settings g_settings;
+
+int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {"show-pids", no_argument, NULL, 'p'},
+    {"version", no_argument, NULL, 'v'},
+    {0, 0, NULL, 0},
+  };
+  int o;
+  while((o = getopt_long(argc, argv, "-pv", table, NULL)) != -1) {
+    switch(o) {
+      case 'v': printf("hello\n"); break;
+      default: exit(-1);
+    }
+  }
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  parse_args(argc, argv);
   assert(!argv[argc]);
   return 0;
 }
