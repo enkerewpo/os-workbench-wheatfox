3fcc65e8c3131b55decf792af87de3a2d81d3248
tracer-nju <tracer@nju.edu.cn>
2024-11-19 15:06:11 +0800

    Linux debian 6.1.0-26-arm64 #1 SMP Debian 6.1.112-1 (2024-09-30) aarch64 GNU/Linux  15:06:11 up  3:45,  1 user,  load average: 0.96, 0.66, 0.50

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -149,7 +149,7 @@ void generate_tree()
 void dfs(int u, int dep)
 {
   for (int i = 0; i < dep; i++)
-    printf("--");
+    printf("\t");
   printf("%s\n", pn[u].name);
   for (int i = 0; i < pn[u].nr_children; i++)
   {
