323a64ae68b37592c4aa80606c6bb5c1c0697027
tracer-nju <tracer@nju.edu.cn>
2024-11-19 11:25:13 +0800

    Linux debian 6.1.0-26-arm64 #1 SMP Debian 6.1.112-1 (2024-09-30) aarch64 GNU/Linux  11:25:13 up  2:13,  1 user,  load average: 0.85, 0.57, 0.33

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
   for (int i = 0; i < argc; i++)
   {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    // printf("argv[%d] = %s\n", i, argv[i]);
   }
   parse_args(argc, argv);
   assert(!argv[argc]);
